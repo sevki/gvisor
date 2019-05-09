97b7ed7d4352a3a0b4c2903985298a2215e6eb00
sevki <s@sevki.org>
2019-05-09 09:11:03 +0100

    bazelless

---
+++ go.mod
@@ -0,0 +1,18 @@
+module gvisor.googlesource.com/gvisor
+
+go 1.12
+
+require (
+	github.com/cenkalti/backoff v2.1.1+incompatible
+	github.com/gofrs/flock v0.7.1
+	github.com/golang/mock v1.3.0
+	github.com/golang/protobuf v1.3.1
+	github.com/google/btree v1.0.0
+	github.com/google/subcommands v1.0.1
+	github.com/kr/pty v1.1.4
+	github.com/opencontainers/runtime-spec v1.0.1
+	github.com/syndtr/gocapability v0.0.0-20180916011248-d98352740cb2
+	github.com/vishvananda/netlink v1.0.0
+	github.com/vishvananda/netns v0.0.0-20180720170159-13995c7128cc // indirect
+	golang.org/x/sys v0.0.0-20190508100423-12bbe5a7a520
+)
--- pkg/sentry/kernel/futex/atomicptr_bucket.go
+++ pkg/sentry/kernel/futex/atomicptr_bucket.go
@@ -1,21 +1,9 @@
-// Copyright 2019 The gVisor Authors.
-//
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// +build ignore
-
-// Package template doesn't exist. This file must be instantiated using the
-// go_template_instance rule in tools/go_generics/defs.bzl.
-package template
+package futex
 
 import (
 	"sync/atomic"
 	"unsafe"
 )
-
-// Value is a required type parameter.
-type Value struct{}
 
 // An AtomicPtr is a pointer to a value of type Value that can be atomically
 // loaded and stored. The zero value of an AtomicPtr represents nil.
@@ -23,17 +11,17 @@ type Value struct{}
 // Note that copying AtomicPtr by value performs a non-atomic read of the
 // stored pointer, which is unsafe if Store() can be called concurrently; in
 // this case, do `dst.Store(src.Load())` instead.
-type AtomicPtr struct {
+type AtomicPtrBucket struct {
 	ptr unsafe.Pointer
 }
 
 // Load returns the value set by the most recent Store. It returns nil if there
 // has been no previous call to Store.
-func (p *AtomicPtr) Load() *Value {
-	return (*Value)(atomic.LoadPointer(&p.ptr))
+func (p *AtomicPtrBucket) Load() *bucket {
+	return (*bucket)(atomic.LoadPointer(&p.ptr))
 }
 
 // Store sets the value returned by Load to x.
-func (p *AtomicPtr) Store(x *Value) {
+func (p *AtomicPtrBucket) Store(x *bucket) {
 	atomic.StorePointer(&p.ptr, (unsafe.Pointer)(x))
 }
